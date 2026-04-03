9ffdaf16d4630684bf0c57d06f6850d2b7f5cdf8
Dmiterev <dmitriy.miterev03@gmail.com>
2026-04-03 11:27:59 +0400

    lab2

--- ProjectGenerator.Api/Program.cs
+++ ProjectGenerator.Api/Program.cs
@@ -8,23 +8,11 @@ builder.AddRedisDistributedCache("cache");
 builder.Services.AddSingleton<ISoftwareProjectGenerator, SoftwareProjectGenerator>();
 builder.Services.AddScoped<ISoftwareProjectService, SoftwareProjectService>();
 
-var trustedUrls = builder.Configuration.GetSection("CorsOrigins").Get<string[]>() ?? [];
-builder.Services.AddCors(options =>
-{
-    options.AddDefaultPolicy(policy =>
-    {
-        policy.WithOrigins(trustedUrls)
-              .WithMethods("GET")
-              .WithHeaders("Content-Type");
-    });
-});
-
 var app = builder.Build();
 
 app.MapDefaultEndpoints();
-app.UseCors();
 
-app.MapGet("/generate", async (int id, ISoftwareProjectService service) =>
+app.MapGet("/api/generate", async (int id, ISoftwareProjectService service) =>
     await service.GetOrGenerate(id));
 
 app.Run();
--- ProjectGenerator/ProjectGenerator.AppHost/AppHost.cs
+++ ProjectGenerator/ProjectGenerator.AppHost/AppHost.cs
@@ -3,11 +3,18 @@ var builder = DistributedApplication.CreateBuilder(args);
 var cache = builder.AddRedis("cache")
     .WithRedisInsight();
 
-var generationApi = builder.AddProject<Projects.ProjectGenerator_Api>("generation-api")
-    .WithReference(cache)
-    .WaitFor(cache);
+var apiGateway = builder.AddProject<Projects.Api_Gateway>("api-gateway");
+
+for (var i = 0; i < 5; i++)
+{
+    var generationApi = builder.AddProject<Projects.ProjectGenerator_Api>($"generation-api-{i}", launchProfileName: null)
+        .WithHttpsEndpoint(5000 + i)
+        .WithReference(cache)
+        .WaitFor(cache);
+    apiGateway.WaitFor(generationApi);
+}
 
 builder.AddProject<Projects.Client_Wasm>("client-wasm")
-    .WaitFor(generationApi);
+    .WaitFor(apiGateway);
 
 builder.Build().Run();
